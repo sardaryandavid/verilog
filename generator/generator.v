784e7e0182406573520d032257d25806dbf3e5cc
sardaryandavid <adella.tevosova@bk.ru>
2024-03-21 19:17:33 +0300

    fixed

--- generator/generator.v
+++ generator/generator.v
@@ -8,12 +8,27 @@ module generator
 reg [2:0] stage;
 
 always @(posedge clk)
-if (reset)
-	{imp, stage} <= 5'h0;
-else begin
-	imp <= (stage[2] == 1) ? ~imp : imp;
-	#1
-	imp = 0;
-	stage <= (stage[2] == 1) ? 3'h0 : stage + 1;
+begin 
+	if (reset) 
+	begin
+		stage <= 3'h0;
+		imp <= 1'b0;
+	end
+	else 
+	begin
+	if (stage == 3'h5) 
+	begin
+		stage <= 3'h0;
+		imp <= 1'b0;
+	end
+	else 
+	begin
+		stage <= stage + 1'b1;
+		imp <= 1'b0;
+	end
 end
+end
+
+assign imp = (stage == 3'h4);
+
 endmodule
